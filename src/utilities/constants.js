f501822e5bb742ab4a940d0011f1af6e6c224f48
duydien0902 <duydien552@gmail.com>
2023-07-05 22:15:49 +0700

    deploy-frontend.

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,7 +2,8 @@
  * Created by trungquandev.com's author on 04/11/2021
  */
 
-export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'http://localhost:8017'
+export const API_ROOT = 'https://duydien-clone-trello.onrender.com'
 // export const API_ROOT = 'https://trello-trungquandev-api.herokuapp.com'
 
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
