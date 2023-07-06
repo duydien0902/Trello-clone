6909f57e41c33ddd036d2ac35c2395f1c8e58f6a
duydien0902 <duydien552@gmail.com>
2023-07-06 19:11:37 +0700

    update

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,12 +3,10 @@ import { env } from '*/config/environtment'
 
 export const corsOptions = {
   origin: function (origin, callback) {
-    // if (!origin && env.BUILD_MODE === 'dev') {
-    //   return callback(null, true)
-    // }
     if (!origin && env.BUILD_MODE === 'production') {
       return callback(null, true)
     }
+
     if (WHITELIST_DOMAINS.indexOf(origin) !== -1) {
       return callback(null, true)
     }
